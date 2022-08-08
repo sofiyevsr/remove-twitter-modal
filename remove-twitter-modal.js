385ec09119561219320dc8ed10e8d463651d7fdb
Rasul <sofiyevsr@gmail.com>
2022-08-08 18:10:01 +0400

    fix: fix check condition

--- remove-twitter-modal.js
+++ remove-twitter-modal.js
@@ -2,11 +2,10 @@ const query = '[data-testid="sheetDialog"]';
 const interval = 400;
 
 const intervalRef = setInterval(() => {
-  if (document.documentElement.style.overflow) {
-    const dialog = document.querySelector(query);
-    if (!dialog) return;
-    document.documentElement.style.overflow = "scroll";
-    dialog.parentElement.remove();
-    clearInterval(intervalRef);
-  }
+  if (document.documentElement.style.overflow !== "hidden") return;
+  const dialog = document.querySelector(query);
+  if (!dialog) return;
+  document.documentElement.style.overflow = "scroll";
+  dialog.parentElement.remove();
+  clearInterval(intervalRef);
 }, interval);
